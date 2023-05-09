fc44d3502402d472c1ead20df9833417e5746177
Mich Seixas <mcseixas@gmail.com>
2023-05-09 19:46:46 +0200

    iteration 4 finished

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -26,7 +26,20 @@ router.post("/celebrities/create", (req, res, next) => {
 });
 
 router.get("/celebrities", (req, res, next) => {
-  res.render("celebrities/celebrities");
+    Celebrity.find() //Used find() method on the Celebrity model to retrieve all the celebrities
+    .then((results) => {
+        const celebrityResults = {results} //transformo el array results en un objeto
+
+        console.log("resultado busqueda" , celebrityResults)
+        return celebrityResults //sin el return no plasma el resultado en la pagina celebrities
+    })
+    .then(results => { // el render tiene que ir dentro de un then, si no no funciona
+        res.render("celebrities/celebrities", results);
+    })
+
+    .catch(err => next(err)) // If there's an error, catch it
+
+  
 });
 
 module.exports = router;
