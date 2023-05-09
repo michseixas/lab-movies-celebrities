766df08d6e508593abcb35a82232d1a6264ad31b
Mich Seixas <mcseixas@gmail.com>
2023-05-09 17:24:03 +0200

    Iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,18 @@
 //  Add your code here
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const celebritySchema = new Schema (
+    {
+        name: {type: String, required: true},
+        occupation: {type: String},
+        catchPhrase: {type: String}
+    }
+);
+ //hasta aqui definimos un Schema (objeto), esto no 
+ //lo tendra en cuenta una BD mientras no sea un modelo.
+ //Para que sea un modelo, se hace así:
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema); //como quiero que la base de datos se refiera("Celebrities"), y como quiero que sea el schema("celebritySchema").
+
+module.exports = Celebrity; 
